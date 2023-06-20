9aab579adbb5bad741a40c4bbe511a9f7a1c5ae5
rmsteja1 <teja.ramisetty@sjsu.edu>
2023-06-19 18:36:59 -0700

    Included ChatBar component into this component.

---
+++ src/component/Chart/Chart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ChatBar from "./ChartBar";
+import './Chart.css';
+
+function Chart(props) {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChatBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Chart;
