1f30bb8a2a5d5e1f4f352d2d81bf2abe6d066cb2
rmsteja1 <teja.ramisetty@sjsu.edu>
2023-06-19 18:26:41 -0700

    Added ExpenseChart component to the Expenses component.

--- src/component/ExpenseItem/Expenses.js
+++ src/component/ExpenseItem/Expenses.js
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import CardFormat from "../UI/CardFormat";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpensesChart";
 
 function Expense(props) {
-  //const expenses = props.expensesArray;
-  const [yearValue, setYearValue] = useState("2020");
+  const [filteredYear, setYearValue] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setYearValue(selectedYear);
-  }; 
+  };
+  const filteredExpenses = props.expensesArray.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <CardFormat className="expenses">
       <ExpensesFilter
-        selected={yearValue}
+        selected={filteredYear}
         onYearSelection={filterChangeHandler}
       />
-      {props.expensesArray.map((eachExpense) => (
-        <ExpenseItem title={eachExpense.title} amount={eachExpense.amount} date={eachExpense.date} />
-      ))}
+      <ExpenseChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
     </CardFormat>
   );
 }
