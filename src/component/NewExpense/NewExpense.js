f0811c5fa36726065c1156e7c431c255434ddf4b
rmsteja1 <teja.ramisetty@sjsu.edu>
2023-06-19 18:33:27 -0700

    Added conditional rendering return operations that supports add new expense operation

--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseFrom";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const SaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false)
   };
+
+  function startEditingHandler() {
+    setIsEditing(true);
+    console.log(isEditing);
+  }
+  function endEditingHandler(){
+    setIsEditing(false);
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} />
+      {!isEditing && <button onClick={startEditingHandler}>Add Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} onCancel={endEditingHandler}/>}
     </div>
   );
 };
