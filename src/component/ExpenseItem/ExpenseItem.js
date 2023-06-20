f94a406df348fa15a32bd581365558575ca6cf39
rmsteja1 <teja.ramisetty@sjsu.edu>
2023-06-19 18:24:15 -0700

    Removed hook for expenseTitle

--- src/component/ExpenseItem/ExpenseItem.js
+++ src/component/ExpenseItem/ExpenseItem.js
@@ -9,19 +9,20 @@ function ExpenseItem(props) {
   // const clickHandler = () => {
   //   setTitle("Updated");
   // };
-  console.log(props.date);
+
   return (
-    <CardFormat className="expense-item">
-      <div>
-        <ExpenseDate date={props.date} />
-      </div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button>Change Title</button>
-      {/* <button onClick={clickHandler}>Change Title</button> */}
-    </CardFormat>
+    <li>
+      <CardFormat className="expense-item">
+        <div>
+          <ExpenseDate date={props.date} />
+        </div>
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button>Change Title</button>
+      </CardFormat>
+    </li>
   );
 }
 
